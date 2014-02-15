c3e3bd774721e92a7bf5bc13501c985c8ab8e65e
henryyang42 <henryyang42@gmail.com>
2014-02-16 03:12:54 +0800

    Update

--- NTHU_OJ_Codes/9063_-_Equation.cpp
+++ NTHU_OJ_Codes/9063_-_Equation.cpp
@@ -1,36 +1,34 @@
-#include<stdio.h>
-#include<map>
-using namespace std;
+#include<cstring>
+#include<cstdio>
 
+int s[5], mul[101], ct;
+unsigned short hash[25000100];
 int main(){
-	long long a, b, c, d, e, ct = 0, ans;
-	while(scanf("%lld%lld%lld%lld%lld", &a, &b, &c, &d, &e) > 0){
-		map<long long, long long> m2;
-		map<long long, long long>::iterator it;
+	for(int i = 0; i <= 100; i++)
+		mul[i] = (i-50)*(i-50)*(i-50);
+	while(scanf("%d%d%d%d%d", s, s+1, s+2, s+3, s+4) > 0){
+		memset(hash, 0, sizeof(hash));
 		for(int i = -50; i <= 50; i++){
-			if(i == 0) continue;
+			if(!i) continue;
 			for(int j = -50; j <= 50; j++){
-				if(j == 0) continue;
+				if(!j) continue;
+				hash[s[3]*mul[i+50]+s[4]*mul[j+50]+12500010]++;
+			}
+		}
+		unsigned long long ans = 0;
+		for(int i = -50; i <= 50; i++){
+			if(!i) continue;
+			for(int j = -50; j <= 50; j++){
+				if(!j) continue;
 				for(int k = -50; k <= 50; k++){
-					if(k == 0) continue;
-					int x = c*i*i*i + d*j*j*j + e*k*k*k;
-					if(m2.find(x) == m2.end())
-						m2[x] = 1;
-					else
-						m2[x]++;
+					if(!k) continue;
+					int x = -(s[0]*mul[i+50]+s[1]*mul[j+50]+s[2]*mul[k+50])+12500010;
+					if(x >= 0 && x < 25000100)
+					ans += hash[x];
 				}
 			}
 		}
-		ans = 0;
-		for(int i = -50; i <= 50; i++){
-			if(i == 0) continue;
-			for(int j = -50; j <= 50; j++){
-				if(j == 0) continue;
-				int x = a*i*i*i + b*j*j*j;
-				ans += m2[x];
-			}
-		}
-		printf("Case %lld: %lld\n", ++ct, ans);
+		printf("Case %d: %llu\n",++ct, ans);
 	}
 	return 0;
 }
